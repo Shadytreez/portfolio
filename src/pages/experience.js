aa173064931536ad03ce1cefac9e9a0e79cbfddd
Truman <trumanngny@yahoo.com>
2021-12-01 16:57:51 -0500

    added projects and experience

--- src/pages/experience.js
+++ src/pages/experience.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Cyborg from '../image/cyborg.png';
 
 class Experience extends React.Component {
    
@@ -9,7 +9,27 @@ class Experience extends React.Component {
 
     render() {
         return (
-            <div>A little info about us.</div>
+            <div>
+                <div class='row'>
+                    <div class="col-sm-6 ">
+                        <div className="container-fluid text-center">
+                        <h2> Cyborg Inc. Internship </h2>
+                        <h3> June 2021 - Aug 2021 </h3>
+                        <a href="Cyborg.co">Cyborg.co </a>
+                        <ul>
+                            <li>Wrote a test bench for Spring Boot server to test REST API responses using Rest Assured, Spring Boot, and Unix4j grep.</li>
+                            <li>Conducted extensive feature testing in order to identify defects in pre-released software in testing environments.</li>
+                            <li>Created the database using Postgres to record the results from the test bench.</li>
+                            <li>Used a service in Linux with terminal commands to increase automation.</li>
+                            <li>Wrote a documentation using Google Docs and ObjectAid UML Explorer to provide an easier understanding of the test bench.</li>
+                        </ul>   
+                        </div>
+                    </div>
+                    <div class="col-sm-6 ">
+                        <img src={Cyborg} class="rounded mx-auto d-block"  width="600" height="200"/>
+                    </div>
+                </div>
+            </div>
           );
     }
 }
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import IndexCard2 from '../image/IndexCard2.jpg';
 
 class Projects extends React.Component {
    
@@ -9,7 +9,30 @@ class Projects extends React.Component {
 
     render() {
         return (
-            <div>A little info about us.</div>
+            <div>
+                <div class='row'>
+                    <div class="col-sm-6 ">
+                        <div className="container-fluid text-center">
+                            <h2> IndexCardMoblieApp </h2>
+                            <a href="https://github.com/Shadytreez/IndexCardMoblieApp">GitHub </a>
+                            <p>The IndexCard app is an moblie application for the android operating system using java.The project creates multiple list of index cards for different topics.Capable to create and view the questions and answers on the index card. Able to test the user on the context of the index card for topic chosen. The purpose of the application is to help people study for certain topic since people always carry their phone. Where the user can create their own question and answer on index cards. Then later going back to study and review for their topic. </p>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 ">
+                        <img src={IndexCard2} class="rounded mx-auto d-block"  width="300" height="600"/>
+                    </div>
+                </div>
+                <div class='row'>
+                    <div class="col-sm-6 ">
+                        <div className="container-fluid text-center">
+                            
+                        </div>
+                    </div>
+                    <div class="col-sm-6 ">
+                        
+                    </div>
+                </div>
+            </div>
           );
     }
 }
